cd87de73f7fef9d4b3365c3c7c52e3b821f171da
A-Bergfeldt <bergfeldt.andreas@gmail.com>
2024-06-06 21:06:37 +0200

    Day 6 solutions

---
+++ day_6/6_2.py
@@ -0,0 +1,28 @@
+import re
+from numpy import roots
+from math import ceil, floor, prod
+
+def quad_solver(xx, x, c):
+    sols = roots([xx, x, c])
+    x_1 = int(ceil(min(sols)))
+    x_2 = int(floor(max(sols)))
+
+    return x_1, x_2
+
+def main():
+
+    res_list = []
+    with open('day_6\\input.txt', 'r') as f:
+        lines = f.readlines()
+        for i in range(len(lines)):
+            lines[i] = re.sub('\D', '', lines[i])
+        min_hold, max_hold = quad_solver(-1, int(lines[0]), -int(lines[1]))
+        n_sols = (max_hold - min_hold) + 1
+        res_list.append(n_sols)
+
+
+    print(f'Product of ways to win = {prod(res_list)}')
+
+
+if __name__ == '__main__':
+    main()
